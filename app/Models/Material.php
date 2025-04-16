272b71b46abe38cdf659316ef6a6ec0a4f07ebd4
Aadya Parasar <67162511+aadya1234-parasar@users.noreply.github.com>
2025-04-16 23:22:53 +0530

    Update Material.php

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -1,26 +1,13 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Material extends Model
 {
-    protected $fillable = ['category_id', 'name', 'opening_balance', 'is_deleted'];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function transactions()
-    {
-        return $this->hasMany(Transaction::class);
-    }
-
-    public function getCurrentBalanceAttribute()
-    {
-        return $this->opening_balance + $this->transactions()->sum('quantity');
-    }
+    protected $fillable = ['name', 'category_id', 'opening_balance'];
 }
+/* code author: AADYA PARASAR */
 
-/* Code Author: AADYA PARASAR */
 
