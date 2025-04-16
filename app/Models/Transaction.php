b98ff8f587b73bf24471267824a30de956b0d37e
Aadya Parasar <67162511+aadya1234-parasar@users.noreply.github.com>
2025-04-16 23:24:36 +0530

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -1,16 +1,1 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Transaction extends Model
-{
-    protected $fillable = ['material_id', 'date', 'quantity'];
-
-    public function material()
-    {
-        return $this->belongsTo(Material::class);
-    }
-}
-
-/* Code Author: AADYA PARASAR */
 
