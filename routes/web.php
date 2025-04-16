e7a99f6135406d34bab29b6e8761c43999b4b17f
Aadya Parasar <67162511+aadya1234-parasar@users.noreply.github.com>
2025-04-16 23:33:17 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\MaterialController;
-use App\Http\Controllers\TransactionController;
 
-Route::get('/', fn() => redirect('/materials'));
-
-// Category Routes
-Route::resource('categories', CategoryController::class);
-
-// Material Routes
-Route::resource('materials', MaterialController::class);
-
-// Transaction Routes
-Route::get('transactions/create', [TransactionController::class, 'create'])->name('transactions.create');
-Route::post('transactions', [TransactionController::class, 'store'])->name('transactions.store');
-
-// Manage Materials View
-Route::get('/manage', [MaterialController::class, 'manage'])->name('materials.manage');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 /* Code Author: AADYA PARASAR */
